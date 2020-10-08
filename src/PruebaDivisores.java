81e559d4f8e87ccfc947571bd91463024679a7c3
Bryan Ezequiel Valdez Calderón <bryan501th@gmail.com>
2020-10-07 21:28:18 -0500

    Se creó la clase PruebaDivisores y la interfaz Validacion

---
+++ src/PruebaDivisores.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+interface Validacion{
+	Scanner input = new Scanner(System.in);
+	public static int validacionNatural() {
+		int ret = 0;
+		boolean err = false;
+		do {
+			try {
+				ret = input.nextInt();
+			} catch (java.util.InputMismatchException e) {
+				System.out.println("entrada no valida, intente de nuevo:");
+				input.nextLine();
+				err=true;
+			}
+			if (ret>0) {
+				err=false;
+			}else {
+				System.out.println("solo números naturales");
+				err=true;
+			}
+		}while(err);
+		return ret;
+	}
+}
+
+
+public class PruebaDivisores {
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
