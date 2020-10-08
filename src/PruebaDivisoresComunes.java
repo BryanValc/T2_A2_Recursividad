6f55630f0d72f4fdabc60b49f6d26ea2c71c6797
Bryan Ezequiel Valdez Calderón <bryan501th@gmail.com>
2020-10-08 00:07:54 -0500

    Se creó el menú en el main de PruebaDivisoresComunes

--- src/PruebaDivisoresComunes.java
+++ src/PruebaDivisoresComunes.java
@@ -14,6 +14,8 @@ class DivisoresComunes{
 			}
 			it+=1;
 			mostrarDivisores(a, b, it, cnt);
+		}else {
+			System.out.println("cantidad de divisores : "+cnt);
 		}
 		
 		
@@ -29,7 +31,36 @@ public class PruebaDivisoresComunes {
 	public static void main(String[] args) {
 		
 		DivisoresComunes dc = new DivisoresComunes();
-		dc.mostrarDivisores(30, 5, 0, 0);
+		
+		boolean salir = false;
+		byte opc=0;
+		int a=0;
+		int b=0;
+		
+		do {
+			System.out.println(" 1)Sacar divisores \n 2)Salir");
+			opc = (byte) Validacion.validacionNatural();
+			switch (opc) {
+			case 1:
+				System.out.println("primer numero:");
+				a = Validacion.validacionNatural();
+				System.out.println("segundo numero:");
+				b = Validacion.validacionNatural();
+				dc.mostrarDivisores(a, b, 0, 0);
+				break;
+			case 2:
+				salir=true;
+				break;
+			default:
+				System.out.println("Opcion no valida");
+				break;
+			}
+			
+			
+		} while (!salir);
+		System.out.println("fin de ejecucion");
+		
+		
 		
 	}
 
